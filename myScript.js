5492db42c27e856dad29a3f7837d5c8ac2c4993c
SalmanSherif <48092247+SalmanSherif@users.noreply.github.com>
2020-05-09 14:36:13 +0200

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,10 +1,19 @@
-"use strict";
-
+//import { handleFormSubmit } from 'form-submission-handler.js';
 var start_session;
 var tweet_select;
 var user_input = [];
-
 var image;
+var tweet_text =
+["@FortniteGame I found a bug with the Choppa.If the tail of the Choppa is in a Box with a stair in it or a 1 by 1 then you hop in the Choppa despawns and send you inder the map and then you can just swim around the die from barrier",
+"fortnite is the best game ever not joking",
+"Bizarro draft: Spouses might be discouraged from streaming Tiger King and the kids might have to forsake Fortnite so GMs have enough bandwidth to make picks. And the NFL has asked cell phone providers to avoid tower work near homes of GMs/coaches.",
+"Unpopular opinion. The minigun should take 2 slots in Fortnite.",
+"Who squading up w me and dropping in on the Travis Scott fortnite concert",
+"@trvisXX bout to make fortnite rage",
+"Fortnite is so fun when ur getting spammed by two miniguns and an rpg #welovefortnite",
+"What are the 3 POIs that you miss the most in Fortnite? Mine: 1 - Tilted (duh) 2- Greasy Grove 3- Paradise Palms",
+"I'm impressed they were able to do the shows without disabling any modes. You can dislike the event all you want, but I will forever be thankful that Fortnite does these things for us. They literally don't have to do this. The drops were great, and I enjoyed it.",
+"just got off the phone with ninja we've decided to add a nurse scrubs skin to fortnite and all the profits go toward corona. so powerful #ltsNotlustAGame"];
 
 function category_submit(){
 
@@ -15,65 +24,64 @@ function category_submit(){
 			localStorage.removeItem("category");
 			localStorage.setItem("category", other_category);
 		}
-
 		loadPage("tweet_intent_1.html");
-
 	} else{
 		alert("\nYou haven't named the category.\n\nPlease fill or select a category to proceed.");
 	}
-
 }
 
 function capValue(input){
-	//alert(input);
 	if (typeof(Storage) !== "undefined") {
-		// Store
 		localStorage.removeItem("tweet_rating");
 		localStorage.setItem("tweet_rating", input);
-		// Retrieve
-		//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
 	}
-	//alert(user_inputs);
-	/*
-	var mysql = require('mysql');
-
-	var con = mysql.createConnection({
-		host: "localhost",
-		user: "yourusername",
-		password: "yourpassword"
-	});
-
-	con.connect(function(err) {
-		if (err) throw err;
-		console.log("Connected!");
-		con.query("CREATE DATABASE mydb", function (err, result) {
-			if (err) throw err;
-			console.log("Database created");
-		});
-	});*/
 }
 
 function rateMore(){
-	var user_inputs;
+	dataSubmit();
+}
 
-	user_inputs = [[
-		"blank",
-		localStorage.getItem("game_selected"),
-		localStorage.getItem("category"),
-		localStorage.getItem("intent_selected"),
-		localStorage.getItem("tweet_rating"),
-	]];
-	
+function dataSubmit(){
+
+	var tempText = getTweetSelectStore()[getRatedPosition()];
+	//alert(tempText);
+
+	document.getElementById("tweet_id").value = tempText;//tweet_text[getTweetSelectStore()[getRatedPosition()]];
+	document.getElementById("game_selected").value = localStorage.getItem("game_selected");
+  document.getElementById("tweet_category").value = localStorage.getItem("category");
+	document.getElementById("tweet_intent").value = localStorage.getItem("intent_selected");
+	document.getElementById("tweet_rate").value = localStorage.getItem("tweet_rating");
+	document.getElementById("honeypot").value;
+
+	var tweet_id = document.getElementById("game_selected").value;
+	var game_selected = document.getElementById("game_selected").value;
+	var tweet_category = document.getElementById("tweet_category").value;
+	var tweet_intent = document.getElementById("tweet_intent").value;
+	var tweet_rate = document.getElementById("tweet_rate").value;
+	var honeypot = document.getElementById("honeypot").value;
+
+	//https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server
+	document.getElementById("send_trigger").click();
+
+	var current_rated_pos = getRatedPosition();
+	setRatedPosition(current_rated_pos + 1 );
+	//alert(current_rated_tweet);
+
+	tweetSelect("option_s_1");
+}
+
+
+function getTweetSelectStore(){
+
+	if (localStorage.hasOwnProperty("tweet_select")){
+		return JSON.parse(localStorage.getItem("tweet_select"));
+	}
 }
 
 function tweetSelectStore(input){
-	// Check browser support
 	if (typeof(Storage) !== "undefined") {
-		// Store
 		localStorage.removeItem("tweet_select");
-		localStorage.setItem("tweet_select", input);
-		// Retrieve
-		//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+		localStorage.setItem("tweet_select", JSON.stringify(input));
 	}
 }
 
@@ -88,11 +96,12 @@ function loadExtras(){
 
 	if(current_page.includes("which_game_1.html")){
 		randomizer1();
-	}else if(current_page.includes("tweet_category_1.html")){
-		randomizer2();
-	}else if(current_page.includes("tweet_intent_1.html")){
-
 	}
+
+	if(current_page.includes("tweet_category_1.html")){
+		randomizer2();
+	}
+
 }
 
 function randomizer2(){
@@ -199,8 +208,11 @@ function randomizer1(){
 
 function loadImg(){
 
-	var toString = localStorage.getItem("tweet_select").toString();
-	var tempString = "images_2" + "//tweet_" + toString + ".png";
+	var toString = getTweetSelectStore();
+	var tempNum = getRatedPosition();
+	//alert(toString[tempNum]);
+
+	var tempString = "images_2" + "//tweet_" + toString[tempNum] + ".png";
 	image = document.getElementById('image_to_rate');
 	image.src = tempString;
 
@@ -219,24 +231,64 @@ function categorySelect(button_id){
 	loadPage("tweet_intent_1.html");
 }
 
-function tweetSelect(button_id){
+//Fisher-Yates Shuffle....https://stackoverflow.com/questions/18806210/generating-non-repeating-random-numbers-in-js
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
 
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function tweetSelect(button_id){
 	if ( button_id == "option_s_1") {
 
-		if (typeof(Storage) !== "undefined") {
-			localStorage.removeItem("rate_start");
-			localStorage.setItem("rate_start", true);
+		var current_pos = 0;
+		var toRand = [1,2,3,4,5,6,7,8,9,10];
+
+		if(getRatedPosition() > 9){
+			setRatedPosition(0);
 		}
 
-		var random_number = Math.floor(Math.random() * 5);
-		tweetSelectStore(random_number + 1);
+		//alert(getRatedPosition());
 
+		if(!localStorage.hasOwnProperty("current_pos")){
+			setRatedPosition(0);
+			toRand = shuffle(toRand);
+			tweetSelectStore(toRand);
+		}else {
+			toRand = getTweetSelectStore();
+			tweetSelectStore(toRand);
+			current_pos = getRatedPosition();
+		}
 		loadPage("which_game_1.html");
 	}
-	// Store
-	// Retrieve
-	//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-	//alert(random_number);
+}
+
+function setRatedPosition(new_pos){
+
+	if (typeof(Storage) !== "undefined") {
+		localStorage.removeItem("current_pos");
+		localStorage.setItem("current_pos", new_pos);
+	}
+}
+
+function getRatedPosition(){
+
+	var temp_num = 0;
+
+	if (typeof(Storage) !== "undefined") {
+		temp_num = parseInt(localStorage.getItem("current_pos"));
+	}
+
+	return temp_num;
 }
 
 function gameSelected(button_id){
@@ -264,9 +316,7 @@ function intentSelected(button_id){
 	}
 
 	location.href = "tweet_rate_1.html";
-	//exportToCsv("data.csv", [user_input])
 }
-
 
 // not my code below Author: Jossef Harush
 // Link: https://jsfiddle.net/jossef/m3rrLzk0/
@@ -311,6 +361,31 @@ function exportToCsv(filename, rows) {
 	}
 }
 
+// author: W3schools Link: https://www.w3schools.com/jsref/met_his_back.asp
+function goBack() {
+	window.history.go(-1);
+}
+
+function access_wiki(){
+	window.open("https://fortnite.fandom.com/wiki/Fortnite_Wiki");
+}
+
+// https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+//https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+/*
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}*/
+
 /*
 jQuery('#shards-custom-slider').customSlider({
 start: [25, 50],
@@ -336,12 +411,3 @@ e.preventDefault();
 alert("dashboard");
 //load dashboard
 });*/
-
-// author: W3schools Link: https://www.w3schools.com/jsref/met_his_back.asp
-function goBack() {
-	window.history.go(-1);
-}
-
-function access_wiki(){
-	window.open("https://fortnite.fandom.com/wiki/Fortnite_Wiki");
-}
